beb4771f56b28de1b19c3c18efcac1743ba530b3
Markus Estermann <Markus.Estermann@leuchterag.ch>
2020-12-02 16:19:57 +0100

    Journey/Section in Komponente hinzugefügt

--- src/SwissTransport/Connections.cs
+++ src/SwissTransport/Connections.cs
@@ -22,6 +22,8 @@ namespace SwissTransport
 
         [JsonProperty("products")]
         public string[] Products { get; set; }
+
+        public List<Section> sections { get; set; }
     }
 
     public class ConnectionPoint
@@ -42,5 +44,27 @@ namespace SwissTransport
         public string Platform { get; set; }
 
         public string RealtimeAvailability { get; set; }
+    }
+
+    public class Section
+    {
+        public Journey journey { get; set; }
+        public object walk { get; set; }
+        //public Departure departure { get; set; }
+        //public Arrival arrival { get; set; }
+    }
+
+    public class Journey
+    {
+        public string name { get; set; }
+        public string category { get; set; }
+        public object subcategory { get; set; }
+        public object categoryCode { get; set; }
+        public string number { get; set; }
+        public string @operator { get; set; }
+        public string to { get; set; }
+        //public List<PassList> passList { get; set; }
+        public object capacity1st { get; set; }
+        public object capacity2nd { get; set; }
     }
 }
